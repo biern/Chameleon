471497c8ebdb629662cd71fd2fb02313ad47f99d
biern <biern.m@gmail.com>
2011-11-10 19:00:30 +0100

    command example

--- tests/command.py
+++ tests/command.py
@@ -4,19 +4,24 @@ import sys
 
 sys.path.insert(0, '../src')
 
-from chameleon import api
+from chameleon import api, parse, Database
 
 
 @api.register
-def test_call(self, product_id, name, cost=1, etc='x', *args):
+def test_call(db, product_id, name, cost=1, etc='x', *args):
     """
     Robi coś bardzo ważnego i przydatnego
 
     :param int product_id: Id produktu
     :param int cost: Koszt
     """
-    print(product_id, name, cost, etc, args)
+    cur = db.cursor()
+    cur.execute("SELECT VERSION()")
+    print cur.fetchone()
 
 
 if __name__ == "__main__":
-    cmd = test_call.shell_eval(sys.argv[1:])
+    # parse()
+    db = Database()
+    db.connect()
+    test_call(db, 1, 2, 3, 4, 5, 6)
