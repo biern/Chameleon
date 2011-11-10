142979a802c941cd2eeba42e589b7fde5e5f2250
biern <biern.m@gmail.com>
2011-11-10 19:10:57 +0100

    example

--- tests/command.py
+++ tests/command.py
@@ -5,6 +5,7 @@ import sys
 sys.path.insert(0, '../src')
 
 from chameleon import api, parse, Database
+from chameleon.commands import add_product
 
 
 @api.register
@@ -17,11 +18,15 @@ def test_call(db, product_id, name, cost=1, etc='x', *args):
     """
     cur = db.cursor()
     cur.execute("SELECT VERSION()")
-    print cur.fetchone()
+    print(cur.fetchone())
 
 
 if __name__ == "__main__":
     # parse()
+    # Domyślne ustawienia bazy danych są zapisane w 'chameleon_conf.py',
+    # można je też przeładować w konstruktorze:
+    # Database(host='localhost', user='root', etc)
     db = Database()
     db.connect()
     test_call(db, 1, 2, 3, 4, 5, 6)
+    add_product(db, title='Nowy produkt', stock=10, value=100)
