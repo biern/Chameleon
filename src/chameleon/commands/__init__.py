d39e373646a02a9ba7d36e5127b289a65bfa0de4
biern <biern.m@gmail.com>
2011-11-10 19:10:27 +0100

    fixed importing commands

--- src/chameleon/commands/__init__.py
+++ src/chameleon/commands/__init__.py
@@ -4,5 +4,5 @@ for filename in os.listdir(os.path.dirname(__file__)):
     if filename.startswith("_") or not filename.endswith(".py"):
         continue
 
-    # exec("import commands.{}".format(os.path.splitext(filename)[0]))
-    __import__("{}".format(os.path.splitext(filename)[0]), globals(), locals())
+    exec("from {0} import {0}".format(os.path.splitext(filename)[0]))
+    # __import__("{}".format(os.path.splitext(filename)[0]), globals(), locals())
