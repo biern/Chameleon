0f10357213c869bba0f41d9b55f505b0056643f2
Kacper Szurek <kacper.szurek@bluepaprica.com>
2011-12-19 23:50:38 +0100

    Add image to product

---
+++ src/chameleon/commands/add_image.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from chameleon import api
+import os
+
+@api.register
+def add_image(db, filename, userid=None):
+    """
+    Add image to gekosale
+    :param str filename:
+	:param int languageid:
+    :param int userid:
+	:return: Image id
+    """
+    
+    # @todo stworzyć miniaturki i umieścic je w katalogach: design/_gallery/*
+                
+    # @todo odczytać mime type pliku i odczytać jego id z tabeli filetype
+    fileTypeId = 12
+                                                                       
+    # Read file extension
+    fileNameWithoutExt, fileExtension = os.path.splitext(filename)
+    
+    # @todo odczytać id rozszerzenia pliku z tabeli fileextension
+    fileExtensionId = 3
+    
+    cur = db.cursor()
+    sql = """
+        INSERT INTO file (
+            idfile, 
+            name, 
+            filetypeid, 
+            fileextensionid, 
+            addid
+        )
+		VALUES
+		(
+		    NULL, 
+		    %(name)s, 
+		    %(filetypeid)s,
+		    %(fileextensionid)s,
+		    %(userid)s 
+		)
+        """
+    data = {}
+    data['name'] = fileNameWithoutExt
+    data['filetypeid'] = fileTypeId
+    data['fileextensionid'] = fileExtensionId
+    data['userid'] = userid
+        
+    cur = db.cursor()
+    cur.execute(sql, data)
+    db.commit()
+    
+    imageid = cur.lastrowid
+    
+    return imageid
---
+++ src/chameleon/commands/product_add_image.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from chameleon import api
+
+
+@api.register
+def product_add_image(db, productid, imageid, ismain=0, userid=None):
+    """
+    Add product to image
+    :param int productid:
+    :param int imageid:
+    :param int ismain: 
+    """
+
+    cur = db.cursor()
+    sql = """
+        DELETE FROM productphoto
+        WHERE
+            productid = %(productid)s
+            AND photoid = %(imageid)s
+        """
+    data = {}
+    data['productid'] = productid
+    data['imageid'] = imageid
+
+    cur = db.cursor()
+    cur.execute(sql, data)
+    db.commit()  
+    
+    if ismain == 1:
+        sql = """
+            UPDATE productphoto
+            SET
+                mainphoto = 0
+            WHERE
+                productid = %(productid)s
+            """
+        data = {}
+        data['productid'] = productid
+
+        cur = db.cursor()
+        cur.execute(sql, data)
+        db.commit()
+    
+    sql = """
+        INSERT INTO productphoto (
+            idproductphoto,
+            productid,
+            photoid,
+            addid,
+            mainphoto
+        )
+        VALUES
+        (
+            NULL,
+            %(productid)s,
+            %(imageid)s,
+            %(userid)s,
+            %(ismain)s
+        )
+        """
+    data = {}
+    data['productid'] = productid
+    data['imageid'] = imageid
+    data['userid'] = userid
+    data['ismain'] = ismain
+    
+    cur = db.cursor()
+    cur.execute(sql, data)
+    db.commit()
