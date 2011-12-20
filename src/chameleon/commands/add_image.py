af371ca64c0cb1ab74e00cfbda379825ef6f6f14
biern <biern.m@gmail.com>
2011-12-20 21:10:12 +0100

    add_image stuff

--- src/chameleon/commands/add_image.py
+++ src/chameleon/commands/add_image.py
@@ -4,8 +4,10 @@ from chameleon import api
 import os
 
 
+# TODO: image_add
+
 @api.register
-def add_image(db, filename, userid=None):
+def add_image(db, filename, userid=None, root_path=None):
     """
     Add image to gekosale
     :param str filename:
@@ -13,15 +15,17 @@ def add_image(db, filename, userid=None):
     :param int userid:
     :return: Image id
     """
+    import Image
 
-    # @todo stworzyć miniaturki i umieścic je w katalogach: design/_gallery/*
+    db.validate('root_path', root_path, 'required')
 
+    gallery_dir = os.path.join(root_path, "design/_gallery")
+
+    img = Image.open(filename)
     # @todo odczytać mime type pliku i odczytać jego id z tabeli filetype
     fileTypeId = 12
 
-    # Read file extension
-    fileNameWithoutExt, fileExtension = os.path.splitext(filename)
-
+    basename, ext = os.path.splitext(filename)
     # @todo odczytać id rozszerzenia pliku z tabeli fileextension
     fileExtensionId = 3
 
@@ -44,7 +48,7 @@ def add_image(db, filename, userid=None):
         )
         """
     data = {}
-    data['name'] = fileNameWithoutExt
+    data['name'] = basename
     data['filetypeid'] = fileTypeId
     data['fileextensionid'] = fileExtensionId
     data['userid'] = userid
@@ -54,5 +58,16 @@ def add_image(db, filename, userid=None):
     db.commit()
 
     imageid = cur.lastrowid
+
+    # Generate thumbnails
+    sizes = (50, 100, 200, 300)
+
+    filename = "{0}{1}".format(imageid, ext)
+    for dim in sizes:
+        path = os.path.join(gallery_dir, "_{1}_{1}".format(dim, dim), filename)
+        print path
+        thumb = img.copy()
+        thumb.thumbnail([dim] * 2, Image.ANTIALIAS)
+        thumb.save(path)
 
     return imageid
